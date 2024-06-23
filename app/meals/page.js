11eaaec53681f6151ba03aa7acfe88f2755e719d
Jabarson Christian <christianjabarson@gmail.com>
2024-06-23 19:37:30 +0530

    commit#13

--- app/meals/page.js
+++ app/meals/page.js
@@ -5,6 +5,11 @@ import classes from './page.module.css';
 import MealsGrid from '@/components/meals/meals-grid';
 import { getMeals } from '@/lib/meals';
 
+export const metadata = {
+  title: "All Meals",
+  description: "Explore All Meals around the globe."
+}
+
 async function Meals() {
   const meals = await getMeals();
 
